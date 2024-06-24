706f6f689deccb98af960d7ab880de6110c8ef73
Tin Jukić <tin.jukic.sb@gmail.com>
2024-06-24 20:46:03 +0200

    Initial project setup and gitignore updated

---
+++ AzikusTask/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AzikusTask
+//
+//  Created by Tin on 24.06.2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
